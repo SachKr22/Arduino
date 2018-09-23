8be48bea18b7b54b4a258e17807e9f21eab5cdf6
Coelan <Coelan@iMac-local.local>
2018-09-23 17:05:27 +0200

    up

--- Projects/MTO-station/Validated/BME280-HC-12_Tx.ino
+++ Projects/MTO-station/Validated/BME280-HC-12_Tx.ino
@@ -3,47 +3,50 @@
 */
 
 #include <SoftwareSerial.h>
-#include "DHT.h"
-#define DHTPIN 7   // DHT22 pin
-#define DHTTYPE DHT22 // DHT 22 (AM2302)
-DHT dht(DHTPIN, DHTTYPE); //déclaration du capteur
+#include "SparkFunBME280.h"
+BME280 bme280;
 SoftwareSerial HC12(10, 11); // HC-12 TX Pin 10, HC-12 RX Pin 11
 
 void setup() {
   Serial.begin(115200);             // Serial port to computer
   HC12.begin(9600);               // Serial port to HC12
-  dht.begin();
+  //configuration du capteur
+  bme280.settings.commInterface = I2C_MODE;
+  bme280.settings.I2CAddress = 0x76;
+  bme280.settings.runMode = 3;
+  bme280.settings.tStandby = 3;
+  bme280.settings.filter = 3;
+  bme280.settings.tempOverSample = 5;
+  bme280.settings.pressOverSample = 5;
+  bme280.settings.humidOverSample = 5;
+  Serial.println("Starting BME280... ");
+  delay(10);  // attente de la mise en route du capteur. 2 ms minimum
+  // chargement de la configuration du capteur
+  bme280.begin();
 
 }
 
 void loop() {
+
+  // DEBUT boucle HC-12
   while (HC12.available()) {        // If HC-12 has data
     Serial.write(HC12.read());      // Send the data to Serial monitor
   }
   while (Serial.available()) {      // If Serial monitor has data
     HC12.write(Serial.read());      // Send that data to HC-12
   }
-  delay(2000);
-  // La lecture du capteur prend 250ms
-  // Les valeurs lues peuvet etre vieilles de jusqu'a 2 secondes (le capteur est lent)
-  float h = dht.readHumidity();//on lit l'hygrometrie
-  float t = dht.readTemperature();//on lit la temperature en celsius (par defaut)
-
-  //On verifie si la lecture a echoue, si oui on quitte la boucle pour recommencer.
-  if (isnan(h) || isnan(t))
-  {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-
-  float hic = dht.computeHeatIndex(t, h, false);
+  // FIN boucle HC-12
 
   //Affichages :
-  HC12.print("Humidite: ");
-  HC12.print(h);
-  HC12.print(" %\t");
-  HC12.print("Temperature: ");
-  HC12.print(t);
-  HC12.println(" *C ");
+  HC12.print("Température: ");
+  HC12.print(bme280.readTempC(), 1);
+  HC12.print(" °C");
+  HC12.print("\t Pression: ");
+  HC12.print(bme280.readFloatPressure() / 100.0F, 1);
+  HC12.print(" HPa");
+  HC12.print("\t humidité relative : ");
+  HC12.print(bme280.readFloatHumidity(), 1);
+  HC12.println(" %");
+  delay(1000);
 
 }
