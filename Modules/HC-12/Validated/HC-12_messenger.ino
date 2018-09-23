ce1fbd44ca14763778c672302a1d71fd64cedbde
Coelan <Coelan@iMac-local.local>
2018-09-23 17:05:17 +0200

    Up HC-12 & BME280

--- Modules/HC-12/Validated/HC-12_messenger.ino
+++ Modules/HC-12/Validated/HC-12_messenger.ino
@@ -14,10 +14,10 @@ void setup() {
 }
 
 void loop() {
-  while (HC12.available()) {        // If HC-12 has data
-    Serial.write(HC12.read());      // Send the data to Serial monitor
+  while (HC12.available()) {        // Si je recois sur via HC-12
+    Serial.write(HC12.read());      // Envoi vers Serial monitor
   }
-  while (Serial.available()) {      // If Serial monitor has data
-    HC12.write(Serial.read());      // Send that data to HC-12
+  while (Serial.available()) {      // Si data sur Serial monitor
+    HC12.write(Serial.read());      // Envoi vers HC-12
   }
 }
